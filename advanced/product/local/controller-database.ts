d81d2ba27656ddce8ec40eba31a85c9eae2cd55e
golas-eteome <golas-eteome@users.noreply.github.com>
2025-11-18 09:49:42 +0000

    Add interface.toml, configuration-sms-database.css, controller-database.ts, product-queries.yaml, shared.java

---
+++ advanced/product/local/controller-database.ts
@@ -0,0 +1,4 @@
+export const config = { runtime: 'edge' };
+
+interface common_fixtureProps { data: string }
+
---
+++ advanced/product/local/shared.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/compute/advanced/product/local/shared.java
+// Generated at: 2025-12-03T23:45:45.287Z
+
